8141ca98c0074d5f2f112f470ff56402972781a5
박진우 <tree6316mte@gmail.com>
2025-02-17 16:24:13 +0900

    [ Feat ] Top Down 파티클 추가

--- Assets/Scripts/TopDown/Weapon/ProjectileController.cs
+++ Assets/Scripts/TopDown/Weapon/ProjectileController.cs
@@ -16,6 +16,8 @@ public class ProjectileController : MonoBehaviour
 
     public bool fxOnDestory = true;
 
+    private ProjectileManager projectileManager;
+
     private void Awake()
     {
         spriteRenderer = GetComponentInChildren<SpriteRenderer>();
@@ -53,8 +55,10 @@ public class ProjectileController : MonoBehaviour
     }
 
 
-    public void Init(Vector2 direction, RangeWeaponHandler weaponHandler)
+    public void Init(Vector2 direction, RangeWeaponHandler weaponHandler, ProjectileManager projectileManager)
     {
+        this.projectileManager = projectileManager;
+
         rangeWeaponHandler = weaponHandler;
 
         this.direction = direction;
@@ -74,6 +78,11 @@ public class ProjectileController : MonoBehaviour
 
     private void DestroyProjectile(Vector3 position, bool createFx)
     {
+        if (createFx)
+        {
+            projectileManager.CreateImpactParticlesAtPostion(position, rangeWeaponHandler);
+        }
+
         Destroy(this.gameObject);
     }
 }
